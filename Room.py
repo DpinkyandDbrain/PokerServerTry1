9cf358c30e34cdb94fe4604375e886f0d6b4aa1a
DpinkyandDbrain <DpinkyandDbrain@yahoo.com>
2015-04-01 16:14:14 -0400

    adding in pre and post server creation and shutdown. This is very powerful, also cleaned up some of the code starting up the rooms and tables.

--- Room.py
+++ Room.py
@@ -1,8 +1,12 @@
-import Table
+from Table import Table
 
 
 class Room:
+    tables = []
 
     def __init__(self, tablenum):
+        if tablenum == 0:
+            tablenum = 8
         for x in range(0, tablenum):
-            self.tables[x] = Table()
+            print 'starting up Table ', x
+            self.tables.append(Table(8))
--- __init__.py
+++ __init__.py
@@ -3,32 +3,64 @@ __author__ = 'DpinkyandDbrain'
 #!/usr/bin/env python
 
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
-import Room
+from Room import Room
 PORT_NUMBER = 8080
+ROOM = None
 
-#This class will handles any incoming request from
-#the browser
+# This class will handles any incoming request from
+# the browser
 
 
-class MyHandler(BaseHTTPRequestHandler):
-	#Handler for the GET requests
+# Subclass HTTPServer with some additional callbacks
+class CallbackHTTPServer(HTTPServer):
+
+    def server_activate(self):
+        ROOM = self.RequestHandlerClass.pre_start()
+        HTTPServer.server_activate(self)
+        self.RequestHandlerClass.post_start()
+
+    def server_close(self):
+        self.RequestHandlerClass.pre_stop()
+        HTTPServer.server_close(self)
+        self.RequestHandlerClass.post_stop()
+
+
+# HTTP request handler
+class PokerJunctionHandler(BaseHTTPRequestHandler):
+
+    @staticmethod
+    def pre_start():
+        print 'Starting up the room'
+        return Room(2)
+
+    @classmethod
+    def post_start(cls):
+        print 'After calling socket.listen()'
+
+    @classmethod
+    def pre_stop(cls):
+        print 'Before calling socket.close()'
+
+    @classmethod
+    def post_stop(cls):
+        print 'After calling socket.close()'
 
     def do_GET(self):
-		self.send_response(200)
-		self.send_header('Content-type','text/html')
-		self.end_headers()
-		# Send the html message
-		self.wfile.write("Hello World !")
-		return
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        # Send the html message
+        self.wfile.write("Hello World !")
+        return
+
 
 try:
-    #Create a web server and define the handler to manage the
-    #incoming request
-    server = HTTPServer(('', PORT_NUMBER), MyHandler)
-    print 'Started httpserver on port ', PORT_NUMBER
-    #Wait forever for incoming htto requests
+    # Create a web server and define the handler to manage the
+    # incoming request
+    server = CallbackHTTPServer(('', PORT_NUMBER), PokerJunctionHandler)
+    print 'The Poker Junction Server has started on port ', PORT_NUMBER
+    # Wait forever for incoming http requests
     server.serve_forever()
-    room = Room(2)
 except KeyboardInterrupt:
-	print 'Interrupt received, shutting down the web server'
-	server.socket.close()
+    print 'Interrupt received, shutting down the web server'
+    server.server_close()
